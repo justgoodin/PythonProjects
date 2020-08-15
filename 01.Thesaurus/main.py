32ceaa8974b2f18e82f8d6d4d1fe2ffbe05bd858
= <=>
2020-08-15 15:51:51 +0530

    NA

--- 01.Thesaurus/main.py
+++ 01.Thesaurus/main.py
@@ -1,5 +1,27 @@
-import translate as ts
+from translator import translate
 
 #User I/O
 word = input("Enter the word: ")
-print(ts.translate(word))
+
+while True:
+    output = translate(word)
+    if output[0] == False:
+        i = 1
+        print(f"{word} means:")
+        for item in output[1]:
+            print(f"{i}. {item}")
+            i += 1
+        break
+    else:
+        print(f"Did you mean \"{output[1]}\"?")
+        print("Enter Y for yes, R to Retry, any other key to exit: ",end="")
+        similar = input().strip()
+        if similar == "Y" or similar == "y":
+            output[0] = False
+            word = output[1]
+        elif similar =="R" or similar == "r":
+            print("Enter the correct word: ",end="")
+            word = input().strip()
+        else:
+            print("Exiting")
+            break
---
+++ 01.Thesaurus/translator.py
@@ -0,0 +1,27 @@
+import json
+import os
+from difflib import get_close_matches
+
+
+filePath = os.path.dirname(__file__)+"/resources/data/data.json" #Defile the path to the data file
+data = json.load(open(filePath,"r")) #Load the json data
+
+#function to match keys to values
+def translate(word):
+    if word.lower() in data:
+        output = data[word.lower()]
+        similar = False
+    elif word.title() in data:
+        output = data[word.title()]
+        similar = False
+    elif word.upper() in data:
+        output = data[word.upper()]
+        similar = False
+    elif len(get_close_matches(word.lower(),data.keys())) > 0:
+        output = get_close_matches(word.lower(),data.keys())[0]
+        similar = True
+    else:
+        output = "The word does not exist"
+        similar = False
+
+    return [similar,output]
