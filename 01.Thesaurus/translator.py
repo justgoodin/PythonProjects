e33f2bbd6afdd523e18b285881915dd789c5e896
= <=>
2020-08-15 16:22:19 +0530

    NA

--- 01.Thesaurus/translator.py
+++ 01.Thesaurus/translator.py
@@ -1,12 +1,33 @@
-import json
-import os
 from difflib import get_close_matches
 
+import mysql.connector
 
+connector = mysql.connector.connect(
+    user = "ardit700_student",
+    password = "ardit700_student",
+    host = "108.167.140.122",
+    database = "ardit700_pm1database"
+)
+
+cursor = connector.cursor()
+
+def translate(word):
+    query = cursor.execute(f"SELECT * FROM Dictionary WHERE Expression ='{word}'")
+
+    results = cursor.fetchall()
+    
+
+    i = 1
+    for result in results:
+        print(f"{i}. {result[1]}")
+        i += 1
+
+
+
+'''
 filePath = os.path.dirname(__file__)+"/resources/data/data.json" #Defile the path to the data file
 data = json.load(open(filePath,"r")) #Load the json data
 
-'''
 #function to match keys to values
 def translate(word):
     if word.lower() in data:
